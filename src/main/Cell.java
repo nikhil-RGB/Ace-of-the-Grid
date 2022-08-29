a9e0dcd361d800dd311dea08fccb5968d55ff5fb
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-08-29 09:29:50 +0530

    Update Cell class

--- src/main/Cell.java
+++ src/main/Cell.java
@@ -2,6 +2,26 @@ package main;
 import java.awt.*;
 public final class Cell 
 {
-	private String type="";
+	private String type="";//type of Ace
+	private int num;//Currently filled cells
+	private int crit_mass;//Maximum mass of a cell
+	private Point point;//Point at which a cell exists
+	private CellGrid parent;//Parent cells.
+	public Cell(int init,Point position,CellGrid parent)
+	{
+		this.num=init;
+		this.point=position;
+		this.parent=parent;
+	}
+	public int getCount()
+	{
+		return this.num;
+	}
+	public int getCritMass()
+	{
+		return this.crit_mass;
+	}
+	
+	
 
 }
---
+++ src/main/CellGrid.java
@@ -0,0 +1,10 @@
+package main;
+
+public final class CellGrid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
